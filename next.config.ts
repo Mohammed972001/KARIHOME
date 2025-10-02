88adfedbb0676a1b91937f40eacc29dc066da38e
Mohmmed Alsayed <Mohammed.94822@cs.mti.edu.eg>
2025-10-02 22:31:33 +0300

    Fix: Restore original config files from FAQs branch + ContactUs features

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,8 @@
-import createNextIntlPlugin from "next-intl/plugin";
+import createNextIntlPlugin from 'next-intl/plugin';
 
-const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
+const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // حل مشكلة File Watching
-  webpack: (config: any) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    };
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default withNextIntl(nextConfig);
